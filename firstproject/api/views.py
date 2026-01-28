c4db47eec10c4ef47f4c4c2be6d079ca97a4751f
joaquim <joaquim.sk21@gmail.com>
2026-01-28 16:07:59 -0300

    Adding endpoints

---
+++ firstproject/api/views.py
@@ -0,0 +1,47 @@
+from django.core.serializers import serialize
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.request import Request
+from rest_framework import status
+from .models import User
+from .serializer import UserSerializer
+
+
+@api_view(['GET'])
+def get_users(_request: Request) -> Response:
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(['POST'])
+def create_user(request: Request) -> Response:
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    else:
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def user_detail(request: Request, pk: int) -> Response:
+    try:
+        user = User.objects.get(pk=pk)
+    except User.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = UserSerializer(user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'PUT':
+        serializer = UserSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    else:
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
